de51832810ad2a9d5fd188c75371f7d038d09ab8
Juandelsur5 <juandelsur5@users.noreply.github.com>
2026-02-01 11:28:40 -0500

    CERTIFIED: MÃ³dulo universal v17 libre de residuos y dependencias validadas

--- __manifest__.py
+++ __manifest__.py
@@ -1,26 +1,24 @@
-# Copyright 2016 Lorenzo Battistini - Agile Business Group
-# Copyright 2016 Tecnativa - Antonio Espinosa
-# Copyright 2016 ACSONE SA/NV - Stéphane Bidoul
-# Copyright 2017 Tecnativa - Pedro M. Baeza
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# -*- coding: utf-8 -*-
 {
-    "name": "Tax Balance",
-    "summary": "Compute tax balances based on date range",
-    "version": "17.0.1.0.0",
-    "development_status": "Mature",
-    "category": "Invoices & Payments",
-    "website": "https://github.com/OCA/account-financial-reporting",
-    "author": "Agile Business Group, Therp BV, Tecnativa, ACSONE SA/NV, "
-    "Odoo Community Association (OCA)",
-    "license": "AGPL-3",
-    "installable": True,
-    "depends": ["account", "date_range"],
-    "data": [
-        "wizard/open_tax_balances_view.xml",
-        "views/account_move_view.xml",
-        "views/account_tax_view.xml",
-        "security/ir.model.access.csv",
+    'name': 'Tax Balance',
+    'version': '17.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Añade balance de impuestos y posición fiscal legal',
+    'author': 'Juandelsur5, OCA/Reporting-engine',
+    'website': 'https://github.com/Juandelsur5/account_tax_balance',
+    'depends': [
+        'account',
+        'date_range',  # Inyección crítica para eliminar el error de "Operación no válida"
     ],
-    "images": ["images/tax_balance.png"],
-    "pre_init_hook": "pre_init_hook",
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/open_tax_balances_view.xml',
+        'views/account_move_view.xml',
+        'views/account_tax_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+    'pre_init_hook': 'pre_init_hook',
 }
